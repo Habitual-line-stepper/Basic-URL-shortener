573f08288e1120875282fdfce0f9e7010a85d553
Josh Warner <44125157+Habitual-line-stepper@users.noreply.github.com>
2023-03-27 13:36:20 +0200

    Update url_short.py

--- url_short.py
+++ url_short.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 import pyshorteners
 import pyperclip
-clipboard_url = []
 
 
 def shorten_url():
@@ -9,23 +8,24 @@ def shorten_url():
     short = pyshorteners.Shortener()
     short_url = short.tinyurl.short(long_url)
     shortened_label.config(text=short_url)
-
-    global clipboard_url
-    clipboard_url = short_url
+    return short_url
 
 
 def copy_to_clipboard():
+    clipboard_url = shorten_url()
     pyperclip.copy(clipboard_url)
+
+
+# Create tkinter widgets here and set callbacks as needed
 
 
 window = tk.Tk()
 window.title("URL Shortener")
-window.geometry("450x100")
 
 url_label = tk.Label(window, text="Enter URL:")
 url_label.grid(column=0, row=0)
 
-url_entry = tk.Entry(window, width=50)
+url_entry = tk.Entry(window, width=30)
 url_entry.grid(column=1, row=0)
 
 shorten_button = tk.Button(window, text="Shorten", command=shorten_url)
